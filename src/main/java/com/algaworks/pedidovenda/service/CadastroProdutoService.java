2884351c393c44eed2c4007a232f525568b86385
marcel.bezerra <marcel.bezerra@arce.ce.gov.br>
2016-08-26 11:21:54 -0300

    Primeiro commit

---
+++ src/main/java/com/algaworks/pedidovenda/service/CadastroProdutoService.java
@@ -0,0 +1,26 @@
+package com.algaworks.pedidovenda.service;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+
+import com.algaworks.pedidovenda.model.Produto;
+import com.algaworks.pedidovenda.repository.Produtos;
+
+public class CadastroProdutoService implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private Produtos produtos;
+	
+	public Produto salvar(Produto produto){	
+		Produto produtoExistente = produtos.porSKU(produto.getSku());
+		
+		if(produtoExistente != null){
+			throw new NegocioException("Já existe um produto com SKU informado");
+		}
+		return produtos.guardar(produto);
+	}
+
+}
