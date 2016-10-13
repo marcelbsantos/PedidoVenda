5c04794cca9da6be709dc9d3556c43a7890de07e
marcel.bezerra <marcel.bezerra@arce.ce.gov.br>
2016-10-13 12:47:44 -0300

    Aula 15.4

--- src/main/java/com/algaworks/pedidovenda/repository/Usuarios.java
+++ src/main/java/com/algaworks/pedidovenda/repository/Usuarios.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 import com.algaworks.pedidovenda.model.Usuario;
 
@@ -22,6 +23,19 @@ public class Usuarios implements Serializable{
 	public List<Usuario> vendedores(){
 		return this.manager.createQuery("from Usuario", Usuario.class)
 				.getResultList(); 
+	}
+
+	public Usuario porEmail(String email) {
+		Usuario usuario = null;
+		
+		try {
+			usuario = this.manager.createQuery("from Usuario where lower(email) = :email", Usuario.class)
+					.setParameter("email", email.toLowerCase()).getSingleResult();			
+		} catch (NoResultException e) {
+			// nenhum usuário encontrado com o e-mail informado
+		}
+		
+		return usuario;
 	}
 
 }
